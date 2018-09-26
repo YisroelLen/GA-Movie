9db757b536f0868768b85d3393509d47cbbb7224
YisroelLen <43618898+YisroelLen@users.noreply.github.com>
2018-09-26 16:49:43 -0400

    Add files via upload

---
+++ Solution.py
@@ -0,0 +1,39 @@
+beeps = 2
+boops = 6
+bop = " "
+print (beeps + boops)
+beeps = 0
+boops = 5
+print (beeps + boops)
+beeps = 9
+boops = 3
+print (beeps + boops)
+beeps = 4
+boops = 8
+print (beeps + boops)
+beeps = 10
+boops = 5
+print (beeps + boops)
+print (bop)
+beeps = 11
+boops = 12
+print (beeps + boops)
+beeps = 5
+boops = 10
+print (beeps + boops)
+beeps = 1
+boops = 17
+print (beeps + boops)
+beeps = 5
+boops = 7
+print (beeps + boops)
+beeps = 4
+boops = 0
+print (beeps + boops)
+print ("1 = A, 2 = B,3 = C,4 = D, 5 = E, 6 = F, 7 = G,")
+print ("8 = H, 9 = I, 10 = J,11 = K, 12 = L, 13 = M")
+print ("14 = N, 15 = O, 16 = P, 17 = Q, 18 = R, 19 = S")
+print ("20 = T, 21 = U, 22 = V, 23 = W, 24 = X")
+print ("25 = Y, 26 = Z")
+print (str(8) + str(5)+ str(12) + str(12) + str(15) + " " + str(23) + str(15) + str(18) + str(12) + str(4))
+print ("HELLO WORLD" )
