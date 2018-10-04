1845e131e5c1634655ee8d75761895fc3a99512b
YisroelLen <43618898+YisroelLen@users.noreply.github.com>
2018-10-04 13:37:35 -0400

    Update DisneyCharacterHw.py

--- DisneyCharacterHw.py
+++ DisneyCharacterHw.py
@@ -2,7 +2,7 @@ disney_characters = ['Cinderella', 'Little Mermaid', 'Elsa', 'Mufasa', 'Simba',"
 for character in disney_characters:
     if "u" in (character):
         print (character + " I bet you're Impressively Intelligent!")
-    if "o" in (character):
+    elif "o" in (character):
         print (character + " O My! How Original!")
     else:
         print (character + " Ehh, a's and e's are so ordinary.")
