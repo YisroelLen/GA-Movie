deeb9f9c1aa0e7f773e77eace764d0704c96e8ec
YisroelLen <43618898+YisroelLen@users.noreply.github.com>
2018-10-04 11:51:18 -0400

    Add files via upload

---
+++ DisneyCharacterHw.py
@@ -0,0 +1,8 @@
+disney_characters = ['Cinderella', 'Little Mermaid', 'Elsa', 'Mufasa', 'Simba',"ariel", "pumba", "flounder", "nala", "ursula", "scar", "flotsam", "timon"]
+for character in disney_characters:
+    if "u" in (character):
+        print (character + " I bet you're Impressively Intelligent!")
+    if "o" in (character):
+        print (character + " O My! How Original!")
+    else:
+        print (character + " Ehh, a's and e's are so ordinary.")
